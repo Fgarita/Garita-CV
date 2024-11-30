89bf0fbd59fef63f1331f673febe43f60be58761
Jose Garita <jgarita40683@ufide.ac.cr>
2024-11-29 23:09:31 -0600

    Add send email

--- scripts/index.js
+++ scripts/index.js
@@ -58,3 +58,87 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+  // Lista de habilidades
+  const skills = [
+    'Scrum/Agile',
+    'Git/GitHub',
+    'ASP.NET Core',
+    'XHTML',
+    'REST APIs',
+    '.NET Framework',
+    'ASP.NET MVC',
+    'Systems auditing',
+    'Oracle Database',
+    'ASP.NET Web API',
+    'LINQ',
+    'SQL Server Management Studio',
+    'Web Design',
+    'Sqlserver',
+    'Data Structure',
+    'Requirements analysis',
+    'Mysql',
+    'Electronics',
+    'CCNA2',
+    'Data Warehouse',
+    'Java Web Services',
+    'Attention to Details',
+    'Teamwork',
+    'Troubleshooting',
+    'Communication',
+    'Microsoft Visual Studio Code',
+    'Microsoft Visual Studio',
+    'Power BI',
+    'Operating systems: Windows, Linux',
+    'Java(basic)',
+    'HTML/CSS(intermediate)',
+    'JavaScript(basic)',
+    'SQL(intermediate)',
+    'C#(intermediate)',
+    'Kotlin(basic)',
+    'PHP(basic)'
+  ];
+
+  // Función para agregar las habilidades al HTML
+  function displaySkills() {
+    const skillsListContainer = document.getElementById('skills-list');
+    
+    // Limpiar el contenedor antes de agregar nuevas habilidades
+    skillsListContainer.innerHTML = '';
+
+    // Recorrer la lista de habilidades y agregar cada una al contenedor
+    skills.forEach(skill => {
+      const skillItem = document.createElement('div');
+      skillItem.classList.add('skills-item');
+      skillItem.innerHTML = `<h2>${skill}</h2>`;
+      skillsListContainer.appendChild(skillItem);
+    });
+  }
+
+  // Llamar a la función para mostrar las habilidades
+displaySkills();
+
+function sendMail(){ 
+  let parms = {
+
+    name: document.getElementById("name").value,
+    email : document.getElementById("email").value,
+    subject : document.getElementById("subject").value,
+    message : document.getElementById("message").value,
+    
+  }
+  
+  // Enviar el correo a través de EmailJS
+  emailjs.send("service_bivy7f6", "template_sk3r8mj", parms)
+    .then(function(response) {
+      // Mostrar mensaje de éxito
+      alert("Email Sent!!");
+
+      // Limpiar el formulario después de enviar el correo
+      document.getElementById("form").reset(); // Aquí 'form' es el id del formulario
+
+    }, function(error) {
+      // Si ocurre un error, mostrar el error
+      alert("Error al enviar el correo: " + JSON.stringify(error));
+    });
+}
